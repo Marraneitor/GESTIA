cea5237f49f639e60f4905ecfc7e9ac3075c5807
Marraneitor <98363026+Marraneitor@users.noreply.github.com>
2026-01-05 01:15:05 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Local Firebase config for MANAGER (manager-d860c)
+// Loaded by index.html via dynamic import.
+export const firebaseConfig = {
+  apiKey: "AIzaSyDnoGvY5cuschts3XqivqOE0-sc4piNaQ4",
+  authDomain: "manager-d860c.firebaseapp.com",
+  projectId: "manager-d860c",
+  storageBucket: "manager-d860c.firebasestorage.app",
+  messagingSenderId: "626095533475",
+  appId: "1:626095533475:web:8204abfbd6454d17effe16",
+  measurementId: "G-43L7VJBLGD"
+};
+
+export default firebaseConfig;
